1a23c62e1508026933a555201f0700be1c35321d
Jaya Vishwakarma <jayavish423@gmail.com>
2023-02-07 13:00:55 +0530

    chakra

---
+++ chakra.py
@@ -0,0 +1,12 @@
+import turtle as t
+t.bgcolor('black')
+t.pencolor('blue')
+t.pensize(4)
+t.up()
+t.goto(-200,0)
+t.down()
+for i in range(36):
+    t.forward(450)
+    t.right((250))
+t.hideturtle()
+t.done()    
